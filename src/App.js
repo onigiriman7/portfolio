2b9cfc0f376a952ce1c15d81bdd5620ecbdae239
PavanS27 <48400643+PavanS27@users.noreply.github.com>
2020-02-05 17:09:19 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,95 +1,34 @@
-import React, {Component} from 'react';
-import Gallery from 'react-photo-gallery'
-import './App.css';
-var images=[
-  {
-    src: 'https://images.pexels.com/photos/3576955/pexels-photo-3576955.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    width: 1,
-    height: 1
-  },
-  {
-    src: 'https://images.pexels.com/photos/3623309/pexels-photo-3623309.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    width: 0.8,
-    height: 1
-  },
-  {
-    src: 'https://images.pexels.com/photos/3415401/pexels-photo-3415401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    width: 3,
-    height: 1
-  },
-  {
-    src: 'https://images.pexels.com/photos/3576955/pexels-photo-3576955.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    width: 0.7,
-    height: 1
-  },
-  {
-    src: 'https://images.pexels.com/photos/3415401/pexels-photo-3415401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    width: 3,
-    height: 1
-  },
-  {
-    src: 'https://images.pexels.com/photos/3046404/pexels-photo-3046404.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    width: 1,
-    height: 1
-  },
-  {
-    src: 'https://images.pexels.com/photos/3415401/pexels-photo-3415401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    width: 3,
-    height: 1
-  },
-  {
-    src: 'https://images.pexels.com/photos/3571551/pexels-photo-3571551.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    width: 1,
-    height: 1
-  },
-  {
-    src: 'https://images.pexels.com/photos/3415401/pexels-photo-3415401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    width: 1,
-    height: 1
-  },
-  {
-    src: 'https://images.pexels.com/photos/3576955/pexels-photo-3576955.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    width: 1,
-    height: 1
-  },
-  {
-    src: 'https://images.pexels.com/photos/3415401/pexels-photo-3415401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    width: 1,
-    height: 1
-  },
-  {
-    src: 'https://images.pexels.com/photos/3576955/pexels-photo-3576955.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    width: 1,
-    height: 1
-  },
+import React, { Component } from "react";
+import "./App.css";
+import Galleria from "./components/Galleria";
+import Contact from "./components/Contact";
 
-]
 function App() {
-  return (
-    <ScrollExample/>
-  );
+  return <ScrollExample />;
 }
 class ScrollExample extends Component {
   render() {
     return (
-     <div>
+      <div>
         <section className="Landing">
-            <h1  style={{margin:0}}>LANDING</h1>
+          <h1 style={{ margin: 0 }}>LANDING</h1>
         </section>
         <section id="portfolio" className="portfolio">
-            <div style={{height:"50vh"}}>
-            <h1  style={{margin:0}}>PORTFOLIO</h1>
-            </div>
+          <div style={{ height: "50vh" }}>
+            <h1 style={{ margin: 0 }}>PORTFOLIO</h1>
+          </div>
         </section>
         <section className="imgGallery">
-            <Gallery photos={images} direction={"column"}/>
+          <Galleria />
         </section>
         <section className="contact">
-
+          <Contact />
         </section>
-     </div>
+      </div>
     );
   }
 }
 
 export default App;
+
+/*  */
---
+++ src/components/Contact.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Fade from "react-reveal/Fade";
+import LightSpeed from "react-reveal/LightSpeed";
+import Rotate from "react-reveal/Rotate";
+
+export default class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col xs={12} md={6}>
+              <Rotate bottom right cascade duration={2000}>
+                <h1 style={{ fontSize: "54px", fontWeight: "700" }}>
+                  Contact Us
+                </h1>
+              </Rotate>
+              <hr
+                style={{
+                  width: 30,
+                  height: 10,
+                  backgroundColor: "black",
+                  float: "left"
+                }}
+              ></hr>
+              <ul>
+                <LightSpeed left delay={2000} duration={1500}>
+                  {" "}
+                  <li className="insta">
+                    <i
+                      className="fa fa-instagram"
+                      style={{ fontSize: "30px" }}
+                    ></i>
+                  </li>
+                </LightSpeed>
+                <LightSpeed left delay={2200} duration={1500}>
+                  <li>
+                    {" "}
+                    <i
+                      className="fa fa-facebook"
+                      style={{ fontSize: "30px" }}
+                    ></i>
+                  </li>
+                </LightSpeed>
+                <LightSpeed left delay={2400} duration={1500}>
+                  <li>
+                    {" "}
+                    <i
+                      className="fa fa-reddit"
+                      style={{ fontSize: "30px" }}
+                    ></i>
+                  </li>
+                </LightSpeed>
+                <LightSpeed left delay={2600} duration={1500}>
+                  <li>
+                    {" "}
+                    <i
+                      className="fa fa-twitter"
+                      style={{ fontSize: "30px" }}
+                    ></i>
+                  </li>
+                </LightSpeed>
+              </ul>
+            </Col>
+            <Col xs={12} md={6} style={{ textAlign: "center" }}>
+              <Fade top duration={2000}>
+                <h1 style={{ fontWeight: "200" }}>Hi There!</h1>
+              </Fade>
+              <Fade top duration={2000}>
+                {" "}
+                <h2 style={{ fontWeight: "200" }}>You look nice today...</h2>
+              </Fade>
+              <Fade right delay={1000}>
+                <h3 style={{ fontSize: "24px", fontWeight: "400" }}>
+                  <i
+                    className="fa fa-phone"
+                    style={{ fontSize: "24px", padding: 15, marginTop: 30 }}
+                  ></i>
+                  : 0000000000
+                </h3>
+              </Fade>
+              <Fade right delay={1500}>
+                <h3 style={{ fontSize: "24px", fontWeight: "400" }}>
+                  {" "}
+                  <i
+                    className="fa fa-envelope"
+                    style={{ fontSize: "24px", padding: 12, marginTop: -30 }}
+                  ></i>
+                  : example@gmail.com
+                </h3>
+              </Fade>
+            </Col>{" "}
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
